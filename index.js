2235d869aad02c363167868381ece7a132f93e9a
Alex Zaitsev <madeinussr91@gmail.com>
2016-04-19 16:10:01 +0300

    edit log message

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get('/', function(req, res) {
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log('currency-server running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
